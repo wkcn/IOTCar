f3515024bac267d4016597ae75de8307774366eb
Zing22 <leezing22@gmail.com>
2017-02-16 21:14:34 +0800

    run at localhost:5050.

--- carIOT.py
+++ carIOT.py
@@ -102,5 +102,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '45.32.48.44', port = 5000)
+    # app.run(debug=True, host = '45.32.48.44', port = 5000)
+    app.run(debug=True, host = '127.0.0.1', port = 5050)
     # app.run(debug=False, port = 80, host = '45.32.56.30')
