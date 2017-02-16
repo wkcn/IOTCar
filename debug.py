d916756d5e0fcce7a16800bc5d342824a723f7c1
Vincent Deng <gdsgdmh@126.com>
2017-02-16 23:28:57 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -7,7 +7,14 @@ def connectToMysql():
 
 def getNLocation():
     cursor = connectToMysql()
-    sql = "SELECT car_id, name, tel, longitude, latitude, oil_capacity, temperature, create_time FROM (SELECT car_id, max(create_time) FROM Drive_info GROUP BY car_id) as dct NATURAL JOIN Drive_info NATURAL JOIN Driver"
+    sql = '''SELECT car_id, name, tel, longitude, latitude, oil_capacity, temperature, create_time
+    FROM Drive_info as t1
+    Join 
+    (SELECT car_id, max(create_time) as create_time from Drive_info group by car_id) as t2
+    ON t1.car_id = t2.car_id AND t1.create_time = t2.create_time
+    Join
+    Driver as t3
+    On t1.driver_id = t3.driver_id'''
     cursor.execute(sql)
     result = cursor.fetchall()
     resultDic = {}
